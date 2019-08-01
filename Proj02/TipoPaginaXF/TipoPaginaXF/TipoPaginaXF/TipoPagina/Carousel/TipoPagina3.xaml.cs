b7fa0d0d19367fc9e68b77f60ebd82cb120fe49e
Jonata Zuanny <jonata.rafael@squadra.com.br>
2019-08-01 13:04:53 -0300

    Projeto 02 - Trabalhando com páginas

---
+++ Proj02/TipoPaginaXF/TipoPaginaXF/TipoPaginaXF/TipoPagina/Carousel/TipoPagina3.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TipoPaginaXF.TipoPagina.Carousel
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TipoPagina3 : ContentPage
+    {
+        public TipoPagina3()
+        {
+            InitializeComponent();
+        }
+
+        private void MudarPagina(object sender, EventArgs args)
+        {
+            //App.Current.MainPage = new NavigationPage(new Navigation.Page1()) { BarBackgroundColor = Color.LightBlue };
+            App.Current.MainPage = new Tabbed.Abas();
+        }
+    }
+}
