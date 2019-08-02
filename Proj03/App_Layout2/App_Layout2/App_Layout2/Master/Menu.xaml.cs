3ddaf1146907b70b36bd529e8af822a70c5587c4
Jonata Zuanny <jonata.rafael@squadra.com.br>
2019-08-02 08:43:17 -0300

    Implementação de Menu lateral com páginas

---
+++ Proj03/App_Layout2/App_Layout2/App_Layout2/Master/Menu.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Layout2.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+    {
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoPaginaPerfil1(object sender, EventArgs args)
+        {
+            Detail = new Pages.Perfil1();
+        }
+
+        private void GoPaginaSobre(object sender, EventArgs args)
+        {
+            Detail = new Pages.Sobre();
+        }
+    }
+}
