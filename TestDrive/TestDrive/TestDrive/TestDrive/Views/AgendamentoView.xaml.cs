24bd7fea754037b36af85e4fb1b00837098eb729
Jonata Rafael <jonatarafael25@gmail.com>
2019-08-05 10:08:37 -0300

    Adicionando projeto TestDrive do primeiro curso de Xamarin do Alura.

---
+++ TestDrive/TestDrive/TestDrive/TestDrive/Views/AgendamentoView.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestDrive.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AgendamentoView : ContentPage
+	{
+
+        public Veiculo Carro { get; set; }
+
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        DateTime dataAg = DateTime.Today;
+        public DateTime DataAgenda {
+            get
+            {
+                return dataAg;
+            }
+            set
+            {
+                dataAg = value;
+            }
+        }
+
+        
+        public TimeSpan HoraAgenda { get; set; }
+        
+        public AgendamentoView (Veiculo car)
+		{
+			InitializeComponent ();
+            this.Carro = car;
+            this.BindingContext = this;
+		}
+
+        private void Agendar(object sender, EventArgs args)
+        {
+            DisplayAlert("Agendamento de Test Drive", string.Format("" +
+@"Cliente: {0}
+Telefone: {1}
+E-mail: {2}
+----------------------------------
+Veículo: {3}
+Agendamento: {4}
+Hora: {5}", Nome, Telefone, Email, Carro.Nome, DataAgenda.ToString("dd/MM/yyyy"), HoraAgenda), "OK");
+        }
+	}
+}
---
+++ TestDrive/TestDrive/TestDrive/TestDrive/Views/ListagemPrincipal.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+    public class Veiculo
+    {
+        public decimal Preco { get; set; }
+        public string PrecoFormatado {
+            get { return string.Format("R$ {0}", Preco); }
+        }
+        public string Nome { get; set; }
+    }
+
+
+    public partial class ListagemPrincipal : ContentPage
+    {
+        public List<Veiculo> Veiculos { get; set; }
+
+        public ListagemPrincipal()
+        {
+            InitializeComponent();
+
+            this.Veiculos = new List<Veiculo>
+            {
+                new Veiculo {Nome = "Onix", Preco = 35400},
+                new Veiculo {Nome = "Uno Fire", Preco = 10400},
+                new Veiculo {Nome = "Astra Sedan", Preco = 27850},
+                new Veiculo {Nome = "Fusion", Preco = 77980},
+                new Veiculo {Nome = "HB20", Preco = 27000},
+                new Veiculo {Nome = "Montana", Preco = 37390},
+                new Veiculo {Nome = "Citröen C4 Pallace", Preco = 43570},
+                new Veiculo {Nome = "Hilux", Preco = 87690}
+            };
+
+            this.BindingContext = this;
+        }
+
+        private void VeiculoSelecionado(object sender, ItemTappedEventArgs args)
+        {
+            var veiculo = (Veiculo)args.Item;
+
+            Navigation.PushAsync(new DetalheView(veiculo));
+
+        }
+    }
+}
