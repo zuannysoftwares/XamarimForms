68e837a4d7ca7011325dc8fa7bcf109f1bc11c71
Jonata Rafael <jonatarafael25@gmail.com>
2019-08-12 11:46:05 -0300

    Aplicando estilos.

---
+++ Proj08/App_Estilo/App_Estilo/App_Estilo/Paginas/DynamicStylePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Estilo.Paginas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DynamicStylePage : ContentPage
+	{
+		public DynamicStylePage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void MudarCor(object sender, EventArgs args)
+        {
+            this.Resources["lblColor"] = Color.Orange;
+            this.Resources["lbl"] = this.Resources["Newlbl"];
+        }
+	}
+}
