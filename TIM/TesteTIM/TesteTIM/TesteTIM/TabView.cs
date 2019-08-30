6e06cd66bca68ab972b193c8a837abca0849f55d
Jonata Rafael <jonatarafael25@gmail.com>
2019-08-30 13:47:16 -0300

    Impelmentação das abas na tela após realizar o login

---
+++ TIM/TesteTIM/TesteTIM/TesteTIM/TabView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TesteTIM
+{
+    [ContentProperty(nameof(ItemSource))]
+    public class TabView : Xam.Plugin.TabView.TabViewControl
+    {
+        public TabView() : base(new List<Xam.Plugin.TabView.TabItem>(), -1)
+        {
+
+        }
+    }
+
+    [ContentProperty(nameof(Content))]
+    public class TabItem : Xam.Plugin.TabView.TabItem
+    {
+        public TabItem() : base("", new ContentView())
+        {
+
+        }
+    }
+}
